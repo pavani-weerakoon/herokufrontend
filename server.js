a59df54f7d6ea82357b07db1e3c37c89842a6694
pavani-weerakoon <66486141+pavani-weerakoon@users.noreply.github.com>
2021-04-21 22:11:42 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.static(__dirname + '/dist/sdgp'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/sdgp/src/index.html'));
+  // res.sendFile(path.join(__dirname+'/public/index.html'));
+  res.sendFile('./index.html');
 });
 
 // Start the app by listening on the default Heroku port
