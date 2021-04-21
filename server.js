c393777a642fcdf6707742b5a238c91ba94ee9c3
pavani-weerakoon <66486141+pavani-weerakoon@users.noreply.github.com>
2021-04-21 22:36:24 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ fs.readdir(path.join(__dirname+'/src'), (err, files) => {
 
 app.get('/*', function(req,res) {
 
-  // res.sendFile(path.join(__dirname+'/public/index.html'));
-  res.sendFile('./index.html');
+  res.sendFile(path.join(__dirname+'/src/index.html'));
+  // res.sendFile('./index.html');
 });
 
 // Start the app by listening on the default Heroku port
